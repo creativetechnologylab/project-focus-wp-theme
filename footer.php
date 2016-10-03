4aedb24e39bca9a3d55be81745885d64ad82439d
garethfoote <gareth.foote@gmail.com>
2016-10-03 16:50:52 +0100

    Added license header to files

--- footer.php
+++ footer.php
@@ -1,9 +1,26 @@
 <?php
+/*
+ *    Copyright (C) 2016 Project Focus Wordpress Theme
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Third party plugins that hijack the theme will call wp_footer() to get the footer template.
  * We use this to end our output buffer (started in header.php) and render into the view/page-plugin.twig template.
  *
- * If you're not using a plugin that requries this behavior (ones that do include Events Calendar Pro and 
+ * If you're not using a plugin that requries this behavior (ones that do include Events Calendar Pro and
  * WooCommerce) you can delete this file and header.php
  */
 
